24a0c308dd7e3ef5166113ee68a3b922d80b1dd2
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2025-11-19 06:00:41 -0800

    c++ files

---
+++ calculator/scientific.h
@@ -0,0 +1,13 @@
+#ifndef SCIENTIFIC_H
+#define SCIENTIFIC_H
+
+#include <cmath>
+
+// Scientific operations
+double power(double base, double exponent);
+double squareRoot(double value);
+double sine(double angle);
+double cosine(double angle);
+double tangent(double angle);
+
+#endif
